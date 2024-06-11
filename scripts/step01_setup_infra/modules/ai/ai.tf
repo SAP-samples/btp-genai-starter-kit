b0f86a28a774b4da71149f28c8618fd557701463
Rui Nogueira <rui1610@users.noreply.github.com>
2024-06-11 16:04:16 +0200

    update TF provider version (#39)

--- scripts/step01_setup_infra/modules/ai/ai.tf
+++ scripts/step01_setup_infra/modules/ai/ai.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     btp = {
       source  = "sap/btp"
-      version = "~> 1.3.0"
+      version = "~> 1.4.0"
     }
   }
 }
--- scripts/step01_setup_infra/provider.tf
+++ scripts/step01_setup_infra/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     btp = {
       source  = "SAP/btp"
-      version = "~> 1.3.0"
+      version = "~> 1.4.0"
     }
   }
 }
